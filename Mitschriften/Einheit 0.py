a7d57793527d369ec2f506e2ca204ec94b463e44
LeButchButch <cheesecaketv53@gmail.com>
2024-10-29 10:04:57 +0100

    Mitschrift Einheit 0

---
+++ Mitschriften/Einheit 0.py
@@ -0,0 +1,11 @@
+import time
+
+print("In diesem Ordner findest du die Mitschriften.")
+print("So musst du selbst nichts mitschreiben")
+print("und kannst dich voll aufs Verständnis konzentrieren.")
+
+for i in list("WAHNSINN!") + ["\nLimitless","Learning","Lounge"]:
+    time.sleep(0.3)
+    print(i)
+
+
