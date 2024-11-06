0c917397967207c5c5d92d54325f476e7735cc61
Eric <cheesecaketv53@gmail.com>
2024-11-06 20:50:49 +0100

    Einheit 01_2.py

---
+++ Mitschriften/Einheit 01_2.py
@@ -0,0 +1,16 @@
+
+x = 5
+y = 10
+
+print(5+9)
+print(5+9, 25)
+print(x, y, x + y)
+
+derString = "Hallo Welt"
+print(derString)
+print("Hallo noch eine Welt")
+
+eingabe = input("Gib etwas ein: ")
+print("Du hast",eingabe,"eingegeben!")
+
+
