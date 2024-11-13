b3b62b8dd689db24f4de0385175dd48bce2b41d5
Eric <cheesecaketv53@gmail.com>
2024-11-13 13:35:22 +0100

    Lösung Übung 2_2

---
+++ Übungen/Lösungen/Übung 2_2.py
@@ -0,0 +1,36 @@
+
+# 1.
+
+eingabe = int(input().strip())
+print(eingabe * 2)
+
+# 2.
+# Problem:
+# Das Fenster schließt sich nach der Eingabe, da das Skript beendet ist.
+# Lösung:
+# input() am Ende anhängen, so wartet das Skript auf eine weitere Eingabe.
+
+# 3.
+
+eingabe = float(input().strip())
+print(int(eingabe + 0.5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
