5878dfb40346207b37ab119038e207955c7bb4fb
Eric <cheesecaketv53@gmail.com>
2024-11-27 20:29:40 +0100

    Übung 4_1.py

--- Übungen/Lösungen/Übung 4_1.py
+++ Übungen/Lösungen/Übung 4_1.py
@@ -1,4 +1,32 @@
 
+# Erstelle die Liste x, welche 10 Zahlen deiner Wahl enthält.
+x = [1,6,4,7,2,3,5,7,1,2]
+
+# Tu im Folgenden so, als ob du die Werte von x nicht kennen würdest.
+
+# Gib den 3., 5., und 7. Wert auf der Konsole aus (muss nicht in der gleichen Zeile sein).
+print(x[3],x[5],x[7])
+
+# Ersetze den 3. Wert durch die Zahl 10.
+x[3] = 10
+
+# Lösche den 4. Wert.
+del x[4]
+
+# Lösche den letzten Wert der Liste.
+del x[-1]
+
+# Hänge den Wert 15 an die Liste an.
+x = x + [15]
+
+# Gib die 5 hintersten Werte auf der Konsole aus.
+print(x[-5:])
+
+
+
+
+
+
 
 
 
